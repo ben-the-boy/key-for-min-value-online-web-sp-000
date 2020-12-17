3e72a8dfe47e26a0ab121539d9a65fcf9ca68388
Ben Hayes <b.t.hayes@comcast.net>
2020-12-17 20:24:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = ""
+  name_hash.collect do |name, value|
+    if value 
+      min_value = name
+    
+  end
 end
